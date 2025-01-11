dbe53aeb559835e66fc16ac4a793533a74126989
Green Tree <126562583+GreenTree09@users.noreply.github.com>
2025-01-11 21:12:26 +0530

    made it more informative

--- setup.py
+++ setup.py
@@ -5,12 +5,14 @@ setup(
     version='1.0.0',
     author='Satya',
     description='Automated Penetration Testing Framework',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     license='MIT',
     packages=find_packages(),
     install_requires=[
         'requests',
         'pandas',
-        # more  to come
+        # more to come
     ],
     include_package_data=True,
     package_data={
@@ -21,4 +23,13 @@ setup(
             'pen-test=src.vulnerability_assessment.assessor:main',
         ],
     },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires='>=3.6',
+    keywords='penetration testing security automation',
 )
